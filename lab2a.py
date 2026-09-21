34bd899332cb2207e49fd36cced5ee54f44439f9
nimatashisherpa25-star <nimatashisherpa25@gmail.com>
2026-09-21 11:22:31 -0400

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -1,8 +1,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Nima Tashi Sherpa
+# Date: 21st sep 2026
 # Purpose: Create a variable, check its type and print the variable.
 # Usage: ./lab2a.py
 
